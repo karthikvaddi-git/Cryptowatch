abe26083cf3e13a3f6899e41ecf2125368b8cfa2
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2021-12-11 08:56:45 +0530

    registration form and basic installations

---
+++ cryptoapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,HttpResponse
+from django.contrib.auth.models import User
+# Create your views here.
+def register(request):
+    if request.method=='GET':
+        return render(request, "registration.html")
+    if request.method=='POST':
+        username=request.POST['email']
+        email=request.POST['email']
+        password=request.POST['psw']
+        user = User.objects.create_user(username=username, email=username, password=password)
+        user.save()
+        return HttpResponse("regiered succesfully")
+
+
+
+
+
+
+
+
+
+
+

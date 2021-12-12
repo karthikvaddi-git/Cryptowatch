02f95f23489a786e0d89d49d306f22b767a29466
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2021-12-12 16:41:13 +0530

    home page in front end

--- cryptoapp/urls.py
+++ cryptoapp/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views as v
 urlpatterns = [
-    path('',v.register,name='register'),
+    path('register',v.register,name='register'),
+    path('',v.home,name='home'),
     path('logintoweb/',v.logintoweb,name='logintoweb'),
     path('cointrack',v.cointrack,name='cointrack')
 
     ]
+
--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home(request):
+     return render(request,"home.html")
+
 @login_required
 def register(request):
     if request.method=='GET':
@@ -39,6 +42,8 @@ def register(request):
 
 
 def logintoweb(request):
+    if request.method=="GET":
+        return render(request,"login.html")
 
     if request.method=='POST':
 

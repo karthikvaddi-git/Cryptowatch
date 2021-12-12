46f8a1090dd4308f5f41dd8b9bfb29243aa038b0
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2021-12-12 16:06:10 +0530

    full twilio whatsapp  bot functionality

--- whatsappbot/views.py
+++ whatsappbot/views.py
@@ -10,6 +10,7 @@ from decouple import config
 from twilio.rest import Client
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
 import json
+from .models import *
 @csrf_exempt
 def bot(request):
     if request.method=='GET':
@@ -27,10 +28,16 @@ def bot(request):
             client = Client("AC1ac2e6dd551790ba9dcd1d3464eab564", "326d9c470a84843106f42f173d203101")
             url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false'
             data = requests.get(url).json()
+            messagetosend = " "
+            i=0;
+            while i < 10:
+                item=data[i]
+                messagetosend = messagetosend + str(item['name']) + '\t' + str(item['current_price']) + '\n'
+                i += 1
+            """
             for item in data:
-                messagetosend=" "
-                messagetosend=messagetosend +str(item['name']) +"  "+str(item['current_price'])
-
+                messagetosend=messagetosend + str(item['name']) + '\t' +str(item['current_price']) + '\n'
+            """
 
             try:
 
@@ -57,10 +64,7 @@ def bot(request):
             string2="https://www.coindesk.com/"
             string3="https://time.com/nextadvisor/investing/cryptocurrency/latest-crypto-news/"
             string4="https://cryptonews.com/"
-
-            for item in data:
-                messagetosend = " "
-                messagetosend = string1  + string2 + string3  + string4
+            messagetosend = string1  + '\n' + string2 + '\n' + string3  + '\n'+ string4
 
 
             try:
@@ -80,13 +84,99 @@ def bot(request):
             except TwilioRestException as e:
                 print(e)
 
+
+        if message == "3":
+            client = Client("AC1ac2e6dd551790ba9dcd1d3464eab564", "326d9c470a84843106f42f173d203101")
+            url='https://api.coingecko.com/api/v3/asset_platforms'
+            data = requests.get(url).json()
+            messagetosend=" "
+
+            for item in data:
+                messagetosend=messagetosend + str(item['name']) + '\n'
+
+
+
+
+            try:
+
+                msd = client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body=messagetosend,
+
+                    to=sender_number
+                )
+
+
+
+
+
+            except TwilioRestException as e:
+                print(e)
+        if message == "4":
+            client = Client("AC1ac2e6dd551790ba9dcd1d3464eab564", "326d9c470a84843106f42f173d203101")
+            url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+            data = requests.get(url).json()
+            string1="coinswitch"
+            string2="coindx"
+            string3="binance"
+            string4="Ftx"
+            messagetosend = string1  + '\n' + string2 + '\n' + string3  + '\n'+ string4
+
+
+            try:
+
+                msd = client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body=messagetosend,
+
+                    to=sender_number
+                )
+                if msd.sid:
+                    print("in 1 block")
+
+
+
+
+            except TwilioRestException as e:
+                print(e)
+
+        if message == "5":
+            client = Client("AC1ac2e6dd551790ba9dcd1d3464eab564", "326d9c470a84843106f42f173d203101")
+
+            string1 = "https://airdrops.io/"
+            string2 = "https://www.binance.com/en/deposit-airdrop"
+            string3 = "https://icomarks.com/airdrops"
+            string4 = "https://airdropalert.com/"
+            messagetosend = string1 + '\n' + string2 + '\n' + string3 + '\n' + string4
+
+            try:
+
+                msd = client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body=messagetosend,
+
+                    to=sender_number
+                )
+                if msd.sid:
+                    print("in 1 block")
+
+
+
+
+            except TwilioRestException as e:
+                print(e)
+
+
         if message == "hi" or message =="HI" or message == "Hi":
             client = Client("AC1ac2e6dd551790ba9dcd1d3464eab564", "326d9c470a84843106f42f173d203101")
             messagetosent= "hello "+sender_name
-            temp=    """  this is your crypto watch bot provides you all crypto market information
-             1.top profit
-             2.top loss
-             3.best defi
+
+            temp=    """ this is your crypto watch bot provides you all crypto market information
+             1.Top 10 crypto market cap coins
+             2.Best websites to follow cryptonews
+             3. Blockchain asset platforms
+             4.Best crypto trading platforms
+             5.Airdrop websites 
             """
             messagetosent=messagetosent + temp
 
